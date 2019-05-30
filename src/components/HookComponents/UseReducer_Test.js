6355d63b34de29917202dab495ba3b443eac0f2d
Manuel <manuelsantiengineering@gmail.com>
2019-05-30 11:19:31 -0400

    Moved the Checkbox list to its own functional class.

--- src/components/HookComponents/UseReducer_Test.js
+++ src/components/HookComponents/UseReducer_Test.js
@@ -5,6 +5,22 @@ import checkboxReducer from "../../reducers/checkboxReducer"
 
 const UseReducer_Test = () => {
 
+    return(
+        <div className="container">
+            <div className="row">
+                <h4> Hooks: useReducer Example </h4>
+                <p>A reducer is simply a function that takes in a previous state as the first parameter,</p>
+                <p>an action as the second parameter, and returns a new state.</p>
+                <p>(previousState, action) => newState </p>
+            </div>
+            <div className="row">
+                <CheckboxList />
+            </div>
+        </div>
+    );
+}
+
+const CheckboxList = () => {
     const initialCheckboxes = [
         {
             id: 'a',
@@ -27,41 +43,22 @@ const UseReducer_Test = () => {
         });
     }
 
-
     return(
-        <div>
-            <h4> Hooks: useReducer Example </h4>
-            <p>A reducer is simply a function that takes in a previous state as the first parameter,</p>
-            <p>an action as the second parameter, and returns a new state.</p>
-            <p>(previousState, action) => newState </p>
-            <div className="container">
-                <div className="row">
-                    <ul className="list-group">
-                        {checkboxList.map( (checkbox) => (
-                            <li className="list-group-item" key={checkbox.id}>
-                                <label>
-                                    <input
-                                        type="checkbox"
-                                        checked={checkbox.complete}
-                                        onChange={() => handleChange(checkbox)}
-                                    />
-                                    {checkbox.task}
-                                </label>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-
-            <input type="checkbox"/>
-                <br/>
-        </div>
+        <ul className="list-group">
+            {checkboxList.map( (checkbox) => (
+                <li className="list-group-item" key={checkbox.id}>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={checkbox.complete}
+                            onChange={() => handleChange(checkbox)}
+                        />
+                        {checkbox.task}
+                    </label>
+                </li>
+            ))}
+        </ul>
     );
 }
-
-const Checkbox = ({}) => {
-
-}
-
 
 export default UseReducer_Test;
